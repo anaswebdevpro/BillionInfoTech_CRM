9af31b4e78f67ecba7a0bdfc3d3cabd3130fd88b
Anas <anaswebdev.pro@gmail.com>
2025-09-18 17:05:38 +0530

    refactor: Remove unused useRef import from Dashboard component to streamline code and improve readability

--- src/pages/Dashboard/Dashboard.tsx
+++ src/pages/Dashboard/Dashboard.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect, useRef} from "react";
+import React, { useState, useEffect} from "react";
 import { COLORS } from "../../constants/colors";
 import { apiRequest } from "../../services";
 
